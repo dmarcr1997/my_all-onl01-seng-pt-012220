b1709b645e26ba07eb5db1ff85087f8198665f5d
Damon R <dmarcr1997@gmail.com>
2020-01-24 23:06:46 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,12 +6,11 @@ def my_all?(collection)
   while i < collection.length
     block_return_values << yield(collection[i])
     i+=1
-    
+  end
     if block_return_values.include?(false)
       false
     else
       true
     end
-  end
 end
 binding.pry
